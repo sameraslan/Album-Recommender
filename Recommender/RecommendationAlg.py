ef0365cd3c1d45532f23b645ba134e14bcd64f44
sameraslan <82460915+sameraslan@users.noreply.github.com>
2022-02-20 15:42:44 -0500

    Gets album artwork given an album

--- Recommender/RecommendationAlg.py
+++ Recommender/RecommendationAlg.py
@@ -1,22 +1,59 @@
 from sklearn.neighbors import NearestNeighbors
-import numpy as np
-import pandas as pd
+from skimage import io
 from tabulate import tabulate
+import matplotlib.pyplot as plt
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import pandas as pd
+import numpy as np
+
+cid = 'c480b13ef81c4e6aa0ab0119636eabe5'
+secret = '50826f24c12044448b906de50ac74742'
+client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 # Prints the 5 most similar albums
 def printSimilar(titles, artists, uris, indices):
     similarAlbumsList = list(indices[90])
     print(distances[1:5])
     albums = []
+    recommendedUris = []
 
     for i in range(len(similarAlbumsList)):
-        albums.append([albumArtists[similarAlbumsList[i]], albumTitles[similarAlbumsList[i]], albumURIs[similarAlbumsList[i]]])
+        albums.append([artists[similarAlbumsList[i]], titles[similarAlbumsList[i]], uris[similarAlbumsList[i]]])
+        recommendedUris.append(uris[similarAlbumsList[i]])
 
     print(tabulate(albums, headers=['Artist', 'Title', 'URI']))
+    visualizeAlbums(recommendedUris)
+
+def visualizeAlbums(uris):
+    urls = []  # List of cover art URLS
+
+    for uri in uris:
+        print(uri)
+        result = sp.album(uri)
+        urls.append(result['images'][0]['url'])  # Append cover art URL to list of image URLS
+
+    plt.figure(figsize=(15, int(0.625 * len(uris))), facecolor='#8cfc03')
+    columns = len(urls)
+
+    for i, url in enumerate(urls):
+        plt.subplot(int(len(urls) / columns + 1), columns, i + 1)
+
+        image = io.imread(url)
+        plt.imshow(image)
+        plt.xticks([])
+        plt.yticks([])
+        s = ''
+        #plt.xlabel(s.join(playlist_df['track_name'].values[i].split(' ')[:4]), fontsize=10, fontweight='bold')
+        plt.tight_layout(h_pad=0.8, w_pad=0)
+        plt.subplots_adjust(wspace=None, hspace=None)
+
+    plt.show()
 
 
 
-# Using unsupervised KNN to get similar albums
+# Using unsupervised KNN to get similar albums (euclidean distance)
 albumsDataframe = pd.read_csv('Spotify API Connection/album_audio_feature_data.csv')
 albumValues = albumsDataframe[['danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo', 'duration_ms', 'time_signature']]
 
