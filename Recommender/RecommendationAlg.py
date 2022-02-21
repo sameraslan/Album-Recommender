7b83b422122ebd52d12da02d541313087dd373af
sameraslan <82460915+sameraslan@users.noreply.github.com>
2022-02-20 21:33:35 -0500

    Got search album working

--- Recommender/RecommendationAlg.py
+++ Recommender/RecommendationAlg.py
@@ -6,6 +6,7 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import pandas as pd
 import numpy as np
+import sys
 
 cid = 'c480b13ef81c4e6aa0ab0119636eabe5'
 secret = '50826f24c12044448b906de50ac74742'
@@ -13,8 +14,8 @@ client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secr
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 # Prints the 5 most similar albums
-def printSimilar(titles, artists, uris, indices):
-    similarAlbumsList = list(indices[90])  # Specified Album to find similar albums to
+def printSimilar(titles, artists, uris, indices, userAlbumIndex):
+    similarAlbumsList = list(indices[userAlbumIndex])  # Specified Album to find similar albums to
     albums = []
     recommendedUris = []
     recommendedTitles = []
@@ -29,6 +30,7 @@ def printSimilar(titles, artists, uris, indices):
     print(tabulate(albums, headers=['Artist', 'Title', 'URI']))
     visualizeAlbums(recommendedUris, recommendedTitles, recommendedArtists)
 
+# Plot similar albums
 def visualizeAlbums(uris, titles, artists):
     urls = []  # List of cover art URLS
 
@@ -47,24 +49,48 @@ def visualizeAlbums(uris, titles, artists):
         plt.xticks([])
         plt.yticks([])
         s = ''
-        plt.xlabel(s.join(artists[i] + ' - ' + titles[i]), fontsize=10, fontweight='bold')
-        plt.tight_layout(h_pad=15, w_pad=15)
+        plt.xlabel(s.join(titles[i] + '\n' + artists[i]), fontsize=8, fontweight='bold')
+        plt.tight_layout(h_pad=15, w_pad=10)
         plt.subplots_adjust(wspace=None, hspace=None)
 
     plt.show()
 
 
+def main(argv):
+    # Using unsupervised KNN to get similar albums (euclidean distance)
+    albumsDataframe = pd.read_csv('Spotify API Connection/album_audio_feature_data.csv')
 
-# Using unsupervised KNN to get similar albums (euclidean distance)
-albumsDataframe = pd.read_csv('Spotify API Connection/album_audio_feature_data.csv')
-albumValues = albumsDataframe[['danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo', 'duration_ms', 'time_signature']]
+    # Get user album index
+    userAlbumIndex = None
+    emptyIndex = True
+    while emptyIndex:
+        albumInput = input("Album title: ")
+        searchResult = albumsDataframe[albumsDataframe['Title'].str.contains(albumInput, na=False)]
+        emptyIndex = searchResult.empty
+        userAlbumIndex = searchResult.index.tolist()[0]
 
-albumTitles = list(albumsDataframe['Title'])  # List of album titles
-albumArtists = list(albumsDataframe['Artist'])  # List of album artists
-albumURIs = list(albumsDataframe['URI'])  # List of album artists
+    # Normalize columns with un-normalized values
+    albumsDataframe[['key', 'loudness', 'tempo', 'duration_ms', 'time_signature']] = (albumsDataframe[['key', 'loudness', 'tempo', 'duration_ms', 'time_signature']] - albumsDataframe[['key', 'loudness', 'tempo', 'duration_ms', 'time_signature']].min()) / (albumsDataframe[['key', 'loudness', 'tempo', 'duration_ms', 'time_signature']].max() - albumsDataframe[['key', 'loudness', 'tempo', 'duration_ms', 'time_signature']].min())
 
-albumValues = albumValues.to_numpy(dtype=np.float32)  # np array of all audio metrics for albums
-similarAlbums = NearestNeighbors(n_neighbors=6, algorithm='auto').fit(albumValues)
-distances, indices = similarAlbums.kneighbors(albumValues)
+    # Selecting audio features for KNN
+    albumValues = albumsDataframe[['danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo', 'duration_ms', 'time_signature']]
 
-printSimilar(albumTitles, albumArtists, albumURIs, indices)  # Print out similar albums
+    # energy, key, mode, speechiness, liveness, tempo, duration_ms, time_signature important
+    # acousticness, instrumentalness, valence, loudness maybe important
+    # acousticness, instrumentalness,  might work
+
+    # Next step is to improve accuracy (potentially use genres)
+    albumValues = albumsDataframe[['loudness', 'energy', 'key', 'mode', 'speechiness', 'liveness', 'tempo', 'duration_ms', 'time_signature']]
+
+    albumTitles = list(albumsDataframe['Title'])  # List of album titles
+    albumArtists = list(albumsDataframe['Artist'])  # List of album artists
+    albumURIs = list(albumsDataframe['URI'])  # List of album artists
+
+    albumValues = albumValues.to_numpy(dtype=np.float32)  # np array of all audio metrics for albums
+    similarAlbums = NearestNeighbors(n_neighbors=6, algorithm='auto').fit(albumValues)
+    distances, indices = similarAlbums.kneighbors(albumValues)
+
+    printSimilar(albumTitles, albumArtists, albumURIs, indices, userAlbumIndex)  # Print out similar albums
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
