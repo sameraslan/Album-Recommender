ee6baa143aaca0f0a58e2eb61f6c9a8da6ad9f35
sameraslan <82460915+sameraslan@users.noreply.github.com>
2022-03-13 14:52:30 -0400

    Creates top 500 map

--- Recommender/MDS_Visualization.py
+++ Recommender/MDS_Visualization.py
@@ -1,16 +1,11 @@
 from sklearn.manifold import MDS
-from matplotlib import pyplot as plt
-import sklearn.datasets as dt
-# import seaborn as sns
 import pandas as pd
 import numpy as np
-from sklearn.metrics.pairwise import manhattan_distances, euclidean_distances
-from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 from bokeh.io import curdoc, show
 from bokeh.models import ColumnDataSource, Grid, ImageURL, LinearAxis, Plot, Range1d
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-import sys
+from sklearn.preprocessing import normalize
 
 cid = 'c480b13ef81c4e6aa0ab0119636eabe5'
 secret = '50826f24c12044448b906de50ac74742'
@@ -19,9 +14,9 @@ sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 urls = []
 
-def readAndCleanData():
+def readAndCleanData(N, b):
     df = pd.read_pickle("Recommender/all_data.pkl")
-    df = df[:100]
+    df = df[:N]
     getURLS(df)
 
     df = df.drop(['URI'], axis=1)  # Drop URI
@@ -46,6 +41,15 @@ def readAndCleanData():
                                                                                                           'duration_ms',
                                                                                                           'time_signature']].min())
 
+    albumValues = df.drop(
+        ['Title', 'Artist', 'Descriptor Count', 'danceability', 'energy', 'key', 'loudness', 'mode',
+         'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo', 'duration_ms',
+         'time_signature'], axis=1)
+    albumValuesCols = albumValues.columns.tolist()
+
+    # Weighting of descriptors (greater b, less weight)
+    df[albumValuesCols] = albumValues[albumValuesCols].apply(lambda x: x / b)
+
     df = df.to_numpy()
     df = np.delete(df, [0, 1, -1], 1)
 
@@ -60,27 +64,25 @@ def getCoordinatesMDS(df):
 
     return X_transform
 
+
 def getURLS(df):
     for uri in list(df['URI']):
         result = sp.album(uri)
         urls.append(result['images'][0]['url'])  # Append cover art URL to list of image URLS
 
+
 def plotAlbums(coordinates):
-    # indices = list(range(len(urls)))
-    # urlDict = dict(zip(indices, urls))  # Creates dictionary of index, url pair
-    # urlDict = {str(k): v for k, v in urlDict.items()}  # Changes keys to string
-    #
-    # source = ColumnDataSource(urlDict)
-    #
-    xdr = Range1d(start=-10, end=10)
-    ydr = Range1d(start=-10, end=10)
+    edge = max(coordinates.max(axis=0))  # Take max of x and y coordinates
+
+    xdr = Range1d(start=-1 * edge, end=edge)
+    ydr = Range1d(start=-1 * edge, end=edge)
 
     plot = Plot(
-        title=None, x_range=xdr, y_range=ydr, width=300, height=300,
+        title=None, x_range=xdr, y_range=ydr, width=3000, height=3000,
         min_border=0, toolbar_location=None)
 
     for i in range(len(coordinates)):
-        plot.add_glyph(ImageURL(url=[urls[i]], x=coordinates[i, 0], y=coordinates[i, 1], w=2, h=2, anchor="bottom_right"))
+        plot.add_glyph(ImageURL(url=[urls[i]], x=coordinates[i, 0], y=coordinates[i, 1], w=.1, h=.1, anchor="bottom_right"))
 
     xaxis = LinearAxis()
     plot.add_layout(xaxis, 'below')
@@ -96,19 +98,8 @@ def plotAlbums(coordinates):
     show(plot)
 
 
-
-
-    # # Just plot dots
-    # fig = plt.figure(2, (10, 4))
-    #
-    # ax = fig.add_subplot(122)
-    # plt.scatter(coordinates[:, 0], coordinates[:, 1])
-    # plt.title('Embedding in 2D')
-    # fig.subplots_adjust(wspace=.4, hspace=0.5)
-    # plt.show()
-
-
-cleanedData = readAndCleanData()
+numAlbums = 500  # How many albums do we want to plot (Top numAlbums on RYM)
+cleanedData = readAndCleanData(numAlbums, b=5)
 coordinates = getCoordinatesMDS(cleanedData)
 plotAlbums(coordinates)
 
